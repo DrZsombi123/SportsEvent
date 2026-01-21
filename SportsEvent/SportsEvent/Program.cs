72a3fdf4506c4e1e918cba61bcfd2a9e86968d80
DrZsombi123 <118626431+DrZsombi123@users.noreply.github.com>
2026-01-21 07:47:37 +0100

    Create Program.cs

---
+++ SportsEvent/SportsEvent/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SportsEvent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
